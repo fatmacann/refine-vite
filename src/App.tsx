e2d6bc28207b9bb685297494f1ffa2e75a926a8a
fatmacann <canf261012@gmail.com>
2024-04-29 11:59:39 +0300

    useUpdate & useList hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Refine } from "@refinedev/core";
 
 import { dataProvider } from "./providers/data-provider";
+
 import { ShowProduct } from "./pages/products/show";
+import { EditProduct } from "./pages/products/edit";
+import { ListProducts } from "./pages/products/list";
 
 export default function App(): JSX.Element {
   return (
     <Refine dataProvider={dataProvider}>
-      <ShowProduct />
+      {/* <ShowProduct /> */}
+      {/* <EditProduct /> */}
+      <ListProducts />
     </Refine>
   );
 }
